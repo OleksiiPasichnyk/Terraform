c40c803264da56779bbd3350635ff9d973848077
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-06-29 18:50:17 -0700

    updates

--- terraform_lesson_10_lb_tf/main.tf
+++ terraform_lesson_10_lb_tf/main.tf
@@ -67,15 +67,93 @@ resource "aws_instance" "test_c6a_large_2" {
   }
 }
 
-#output "web-address_test_instance" {
-#  value = aws_instance.test.public_dns
-#}
+resource "aws_lb" "my_test_front_end" {
+  name                       = "test-lb-lesson-10"
+  internal                   = false
+  load_balancer_type         = "network"
+  subnets                    = [for each in data.aws_subnets.vpcsubnets.ids : each]
+  enable_deletion_protection = false
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "test-target-group"
+  port     = 3000
+  protocol = "TCP"
+  vpc_id   = data.aws_vpc.main.id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    port                = "3000"
+    protocol            = "TCP"
+    unhealthy_threshold = 2
+  }
+}
+
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.my_test_front_end.arn
+  port              = "80"
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "test_1" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test_c6a_large_1.id
+  port             = 3000
+}
+
+resource "aws_lb_target_group_attachment" "test_2" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test_c6a_large_2.id
+  port             = 3000
+}
+
+data "aws_vpc" "main" {
+  default = true
+}
+
+data "aws_subnets" "vpcsubnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+  filter {
+    name   = "default-for-az"
+    values = [true]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+data "aws_subnet" "vpcsubnet" {
+  for_each = { for index, subnetid in data.aws_subnets.vpcsubnets.ids : index => subnetid }
+  id       = each.value
+}
+
+
 output "web-address_test_instance_1" {
-  value = aws_instance.test_c6a_large_1
+  value = aws_instance.test_c6a_large_1.public_dns
 }
 
 output "web-address_test_instance_2" {
-  value = aws_instance.test_c6a_large_2
+  value = aws_instance.test_c6a_large_2.public_dns
+}
+
+output "web-address_lb" {
+  value = aws_lb.my_test_front_end.dns_name
+}
+
+output "web-address_lb-zone-id-for-route53" {
+  value = aws_lb.my_test_front_end.zone_id
 }
 
 data "aws_caller_identity" "current" {}
