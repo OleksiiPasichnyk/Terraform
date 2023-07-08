445059531b9245892bfdfe30e2c7782df4462a58
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-07-08 10:38:24 -0700

    updates to the lesson

---
+++ jenkins_tf_lesson_14/TF_Pipeline.groovy
@@ -0,0 +1,35 @@
+pipeline {
+   agent any
+   tools { terraform "latest" }
+   tools { github ""}
+   parameters {
+          string(name: 'key_id', defaultValue: 'null', description: 'your aws access key id')
+          string(name: 'key_value', defaultValue: 'null', description: 'your aws access key value')
+           }
+   stages{
+      stage ('clone git repo') {
+        steps {
+          git (url: 'https://github.com/OleksiiPasichnyk/Terraform.git', credentialsId: 'MyPrivateGithub' )
+        }
+      }
+      stage ('Plan') {
+        steps {
+          sh 'cd Terraform/jenkins_tf_lesson_14/terraform_infra'
+          sh 'echo "variable "key_id"=${params.key_id}" > variables.tf'
+          sh 'echo "variable "key_value"=${params.key_value}" >> variables.tf'
+          sh 'cat variables.tf'
+          sh 'terraform plan -out=terraform.tfplan'
+        }
+      }
+      stage('Plan verification and user input') {
+        steps {
+            input(message: 'Proceed or abort?', ok: 'Proceed', parameters: [[$class: 'AbortException', name: 'Abort']])
+        }
+    }
+      stage ('Apply') {
+        steps {
+          sh 'terraform apply terraform.tfplan'
+        }
+      }
+    }
+  }
