8342a53bc8c88454fbe5785a10592e9999ab2bdb
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-07-09 20:33:57 -0700

    updated

--- jenkins_tf_lesson_14/terraform_infra/instances.tf
+++ jenkins_tf_lesson_14/terraform_infra/instances.tf
@@ -1,9 +1,9 @@
 
 resource "aws_instance" "test_c6a_large_1" {
   ami                    = "ami-053b0d53c279acc90" #Ubuntu
-  instance_type          = "c6a.large"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  key_name               = "lesson_7_ansible"
+  #key_name               = "lesson_7_ansible"
   tags = {
     Name = "Test_1"
   }
@@ -11,9 +11,9 @@ resource "aws_instance" "test_c6a_large_1" {
 
 resource "aws_instance" "test_c6a_large_2" {
   ami                    = "ami-053b0d53c279acc90" #Ubuntu
-  instance_type          = "c6a.large"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  key_name               = "lesson_7_ansible"
+  #key_name               = "lesson_7_ansible"
   tags = {
     Name = "Test_2"
   }
