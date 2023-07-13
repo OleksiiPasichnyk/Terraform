35383b2b156cb4df7ff8c391cf76d0428ae1f58b
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-07-12 20:53:03 -0700

    updates

--- jenkins_tf_s3_lesson_15/TF_Pipeline_Turn_Off_Infra.groovy
+++ jenkins_tf_s3_lesson_15/TF_Pipeline_Turn_Off_Infra.groovy
@@ -9,21 +9,24 @@ pipeline {
         git(branch: 'main', url: 'git@github.com:OleksiiPasichnyk/Terraform.git', credentialsId: 'access_to_git')
       }
     }
-    
     stage('Destroy') {
       steps {
+        withCredentials([
+        usernamePassword(credentialsId: 'simple_creds_aws', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')
+        ]) {
         sh '''
         aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
         aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-        cd ./jenkins_tf_lesson_15/terraform_infra/
+        cd ./jenkins_tf_s3_lesson_15/terraform_infra/
         terraform plan -destroy -out destroyplan.tfplan
         '''
         input message: 'proceed or abort?', ok: 'ok'
         sh '''
-        cd ./jenkins_tf_lesson_15/terraform_infra/
+        cd ./jenkins_tf_s3_lesson_15/terraform_infra/
         terraform apply destroyplan.tfplan
         '''
        }
-     }
+      }
+    }
   }
 }
