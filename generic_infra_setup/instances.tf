94a3d7601750ef64db032974b3c161a85e87448b
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-09-02 18:20:26 -0700

    update with generic instance setup and docs

---
+++ generic_infra_setup/instances.tf
@@ -0,0 +1,64 @@
+
+resource "aws_instance" "test_c6a_large_1" {
+  ami                    = "ami-053b0d53c279acc90" #Ubuntu
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = "lesson_7_ansible"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo '<!DOCTYPE html>
+<html>
+<head>
+<style>
+  body {
+    background-color: #A1B0FF; /* Replace with your desired background color */
+  	text-align: center; /* Center-align text within the body */
+  }
+</style>
+</head>
+<body>
+  <h1>Hello, Team.  </h1>
+    <h1>Welcome to server 2!</h1>
+</body>
+</html>' > /var/www/html/index.html
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+
+  tags = {
+    Name = "Test_1"
+  }
+}
+
+resource "aws_instance" "test_c6a_large_2" {
+  ami                    = "ami-053b0d53c279acc90" #Ubuntu
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = "lesson_7_ansible"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo '<!DOCTYPE html>
+<html>
+<head>
+<style>
+  body {
+    background-color: #A1B0FF; /* Replace with your desired background color */
+  	text-align: center; /* Center-align text within the body */
+  }
+</style>
+</head>
+<body>
+  <h1>Hello, Team.  </h1>
+    <h1>Welcome to server 2!</h1>
+</body>
+</html>' > /var/www/html/index.html
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+
+  tags = {
+    Name = "Test_2"
+  }
+}
--- generic_infra_setup/sg.tf
+++ generic_infra_setup/sg.tf
@@ -3,14 +3,14 @@ resource "random_pet" "sg" {}
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 0
-    to_port     = 65000
+    from_port   = 3000
+    to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = 0
-    to_port     = 65000
+    from_port   = 3000
+    to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
