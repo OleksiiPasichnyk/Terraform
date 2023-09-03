e74609d605ee6d8c6490213df94f6238869da4a4
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-09-02 19:03:44 -0700

    updates

--- extra_jenkins_setup&jobs/TF_Pipeline_Turn_Off_Infra.groovy
+++ extra_jenkins_setup&jobs/TF_Pipeline_Turn_Off_Infra.groovy
@@ -12,21 +12,29 @@ pipeline {
     stage('Destroy') {
       steps {
         withCredentials([
-        usernamePassword(credentialsId: 'simple_creds_aws', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')
+          usernamePassword(credentialsId: 'simple_creds_aws', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')
         ]) {
-        sh '''
-        aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-        aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-        cd ./generic_infra_setup/
-        terraform plan -destroy -out destroyplan.tfplan
-        '''
-        input message: 'proceed or abort?', ok: 'ok'
-        sh '''
-        cd ./generic_infra_setup/
-        terraform apply destroyplan.tfplan
-        '''
-       }
+          sh '''
+          aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+          aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+          cd ./generic_infra_setup/
+          terraform init
+          terraform plan -destroy -out destroyplan.tfplan || error "Terraform plan failed"
+          '''
+          input message: 'Proceed to destroy resources?', ok: 'Destroy'
+          sh '''
+          cd ./generic_infra_setup/
+          terraform apply destroyplan.tfplan || error "Terraform apply failed"
+          '''
+          sh 'rm destroyplan.tfplan'
+          sh 'aws configure unset aws_access_key_id aws_secret_access_key'
+        }
       }
+    }
+  }
+  post {
+    failure {
+      mail to: 'admin@example.com', subject: 'Terraform Destroy Failed', body: 'The Terraform destroy pipeline failed.'
     }
   }
 }
