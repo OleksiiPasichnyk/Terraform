d205ed0530a17ab86a489b2f16d1d14527cf7289
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-09-09 18:03:52 -0700

    added arm type

--- generic_infra_setup/instances.tf
+++ generic_infra_setup/instances.tf
@@ -1,6 +1,7 @@
 
 resource "aws_instance" "test_c6a_large_1" {
-  ami                    = "ami-053b0d53c279acc90" #Ubuntu
+  ami                    = "ami-0a0c8eebcdd6dcbd0" # ubuntu arm64 
+  # ami                  = "ami-053b0d53c279acc90" # Ubuntu amd64 (x86_64)
   instance_type          = "t4g.small"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "lesson_7_ansible"
