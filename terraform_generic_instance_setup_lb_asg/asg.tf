0f0da49f31427ba0d7a2beec107b2ddf24db6f80
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-10-15 17:43:01 -0700

    updated lesson with asg

--- terraform_generic_instance_setup_lb_asg/asg.tf
+++ terraform_generic_instance_setup_lb_asg/asg.tf
@@ -1,32 +1,31 @@
-
-resource "aws_launch_configuration" "as_conf" {
-  name          = "asg_test_instance"
-  image_id      = "ami-053b0d53c279acc90" //Ubuntu Server 20.04 LTS (HVM), SSD Volume Type // Amazon Machine Image e.g. [ami-053b0d53c279acc90]
-  instance_type = "c5.large"
-  key_name      = "lesson_7_ansible"
-  security_groups = [aws_security_group.web-sg.id]
-}
-
-resource "aws_placement_group" "test_lesson_asg" {
-  name     = "test_lesson_asg"
-  strategy = "cluster"
+resource "aws_launch_template" "as_conf" {
+  name = "test_launch_template"
+  image_id = "ami-053b0d53c279acc90"
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "c6a.large"
+  # key_name = "lesson_7_ansible"
+  vpc_security_group_ids = [
+    aws_security_group.web-sg.id
+  ]
 }
 
 
 resource "aws_autoscaling_group" "c6a_large" {
   name                      = "c6a-terraform-test"
-  max_size                  = 1
+  max_size                  = 3
   min_size                  = 1
   desired_capacity          = 1
   health_check_grace_period = 30
   health_check_type         = "EC2"
   force_delete              = true
-  placement_group           = aws_placement_group.test_lesson_asg.id
-  launch_configuration      = aws_launch_configuration.as_conf.name
+  launch_template {
+    id      = aws_launch_template.as_conf.id
+    version = "$Latest"
+  }
   vpc_zone_identifier       = [for each in data.aws_subnets.vpcsubnets.ids : each]
 
   initial_lifecycle_hook {
-    name                 = "foobar"
+    name                 = "test_lesson_asg"
     default_result       = "CONTINUE"
     heartbeat_timeout    = 120
     lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
@@ -45,7 +44,7 @@ resource "aws_autoscaling_group" "c6a_large" {
 }
 
 resource "aws_autoscaling_policy" "cpu_policy" {
-  autoscaling_group_name = aws_autoscaling_group.c5_large.name
+  autoscaling_group_name = aws_autoscaling_group.c6a_large.name
   name                   = "cpu_bound"
   policy_type            = "TargetTrackingScaling"
   estimated_instance_warmup = 120
