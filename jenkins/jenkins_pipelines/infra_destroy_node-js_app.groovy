389585c68495bef7d2a8a5e5ab3e74955f26f37a
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-10-29 19:23:04 -0700

    updated destroy

--- jenkins/jenkins_pipelines/infra_destroy_node-js_app.groovy
+++ jenkins/jenkins_pipelines/infra_destroy_node-js_app.groovy
@@ -1,29 +1,39 @@
 pipeline {
-  agent any
-  tools {
-    terraform "latest"
-  }
-  stages {
-    stage('Clone Git repo') {
-      steps {
-        git(branch: 'main', url: 'git@github.com:OleksiiPasichnyk/Terraform.git', credentialsId: 'access_to_git')
-      }
+    agent any
+    tools {
+        terraform 'tf1.6'
     }
-    stage('Destroy') {
-      steps {
-         {
-        sh '''
-        cd ./jenkins_tf_s3_lesson_15/terraform_infra/
-        terraform init
-        terraform plan -destroy -out destroyplan.tfplan
-        '''
-        input message: 'proceed or abort?', ok: 'ok'
-        sh '''
-        cd ./jenkins_tf_s3_lesson_15/terraform_infra/
-        terraform apply destroyplan.tfplan
-        '''
-       }
-      }
-     }
-  }
+    stages {
+        stage('Clone Git repo') {
+            steps {
+                git(
+                branch: 'jenkins_instance_setup',
+                url: 'https://github.com/OleksiiPasichnyk/Terraform.git',
+                credentialsId: 'access_to_git'
+                )
+            }
+        }
+        stage('Terraform Initialize and Plan Destroy') {
+            steps {
+                sh '''
+                cd ./jenkins/terraform_ansible_generic_instace_setup_template
+                terraform init
+                terraform plan -destroy -out=destroyplan.tfplan
+                '''
+            }
+        }
+        stage('Plan verification and user input for Destroy') {
+            steps {
+                input message: 'proceed or abort?', ok: 'ok'
+            }
+        }
+        stage('Terraform Apply Destroy') {
+            steps {
+                sh '''
+                cd ./jenkins/terraform_ansible_generic_instace_setup_template
+                terraform apply destroyplan.tfplan
+                '''
+            }
+        }
+    }
 }
