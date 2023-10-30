839874cc02bf5b48b0f49e2c781829e28effbee4
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-10-29 20:04:56 -0700

    upd

--- jenkins/terraform_ansible_generic_instace_setup_template/main.tf
+++ jenkins/terraform_ansible_generic_instace_setup_template/main.tf
@@ -11,11 +11,11 @@ terraform {
   }
 
   required_version = ">= 1.3"
-  backend "s3" {
-    bucket = "terraform-state-test-my-cloud"
-    key    = "nodejsapp_example/terraform.tfstate"
-    region = "us-east-1"
-  }
+  # backend "s3" {
+  #   bucket = "terraform-state-test-my-cloud"
+  #   key    = "nodejsapp_example/terraform.tfstate"
+  #   region = "us-east-1"
+  # }
 }
 
 provider "aws" {
