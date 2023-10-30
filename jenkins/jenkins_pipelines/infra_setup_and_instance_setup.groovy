f66fe546b69d3e27642ff5e85af1c757c3c290b4
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-10-29 19:00:40 -0700

    updates to the pipeline

--- jenkins/jenkins_pipelines/infra_setup_and_instance_setup.groovy
+++ jenkins/jenkins_pipelines/infra_setup_and_instance_setup.groovy
@@ -7,23 +7,43 @@ pipeline {
     stages {
         stage('Clone Git repo') {
             steps {
-                git(branch: 'jenkins_instance_setup', url: 'https://github.com/OleksiiPasichnyk/Terraform.git', credentialsId: 'access_to_git')
+                git(
+                    branch: 'jenkins_instance_setup', 
+                    url: 'https://github.com/OleksiiPasichnyk/Terraform.git', 
+                    credentialsId: 'access_to_git'
+                )
             }
         }
+        
+        stage('Install Ansible') {
+            steps {
+        sh '''
+        sudo apt-add-repository ppa:ansible/ansible -y
+        sudo apt-get update
+        sudo apt-get install ansible -y
+        '''
+            }
+        }
+
         stage('Terraform Plan') {
             steps {
                 sh '''
                 cd ./jenkins/terraform_ansible_generic_instace_setup_template
-                terraform init
+                echo "yes" | terraform init
                 terraform plan -out=terraform.tfplan
                 '''
             }
         }
+
         stage('Plan verification and user input') {
             steps {
-                input message: 'proceed or abort?', ok: 'ok'
+                input(
+                    message: 'proceed or abort?', 
+                    ok: 'ok'
+                )
             }
         }
+
         stage('Terraform Apply') {
             steps {
                 sh '''
@@ -32,20 +52,23 @@ pipeline {
                 '''
             }
         }
+
         stage('Get Terraform Outputs') {
             steps {
                 sh '''
                 cd ./jenkins/terraform_ansible_generic_instace_setup_template
-                terraform output instance_ip > ./ansible/instance_ip.txt
+                terraform output web-address-nodejs > ./ansible/instance_ip.txt
                 '''
             }
         }
+
         stage('Run Ansible') {
             steps {
                 withCredentials([sshUserPrivateKey(credentialsId: 'access_for_new_node_js_app', keyFileVariable: 'SSH_KEY')]) {
                     sh '''
+                    sleep 180
                     cd ./jenkins/terraform_ansible_generic_instace_setup_template/ansible
-                    ansible-playbook -i instance_ip.txt playbook.yml --private-key=$SSH_KEY
+                    ansible-playbook -i instance_ip.txt playbook_apache.yaml -u ubuntu --private-key=$SSH_KEY -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
                     '''
                 }
             }
