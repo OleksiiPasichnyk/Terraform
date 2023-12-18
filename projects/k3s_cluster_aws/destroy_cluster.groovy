4850a551a4de0a74d0ec58d4e53a93ca2b0ebb2e
OleksiiPasichnyk <axelpasechnik@gmail.com>
2023-12-17 23:28:32 -0800

    pipelines updates

---
+++ projects/k3s_cluster_aws/destroy_cluster.groovy
@@ -0,0 +1,104 @@
+pipeline {
+    options {
+        ansiColor('xterm')
+    }
+    agent any
+    tools {
+        terraform 'tf1.6'
+    }
+
+    stages {
+        stage('Sparse Checkout') {
+            steps {
+                script {
+                    checkout([$class: 'GitSCM', 
+                              branches: [[name: 'main']],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions: [[
+                                  $class: 'SparseCheckoutPaths', 
+                                  sparseCheckoutPaths: [[path: 'projects/k3s_cluster_aws/cluster_init/']]
+                              ]],
+                              userRemoteConfigs: [[
+                                  url: 'https://github.com/OleksiiPasichnyk/Terraform.git'
+                              ]]
+                    ])
+                }
+            }
+        }
+        stage('Terraform Plan Destroy Worker Nodes') {
+            steps {
+                sh '''
+                cd ./projects/k3s_cluster_aws/cluster_init/terraform/worker_node_config
+                terraform init -input=false
+                terraform plan -destroy -out=terraform_destroy.tfplan
+                '''
+            }
+        }
+        stage('Approval for Destroy Worker Nodes') {
+            steps {
+                input(
+                    message: 'Review the destroy plan. Proceed with destroy?',
+                    ok: 'Proceed'
+                )
+            }
+        }
+        stage('Terraform Apply Destroy Worker Nodes') {
+            steps {
+                sh '''
+                cd ./projects/k3s_cluster_aws/cluster_init/terraform/worker_node_config
+                terraform apply -input=false terraform_destroy.tfplan
+                '''
+            }
+        }
+        stage('Terraform Plan Destroy Master Node(s)') {
+            steps {
+                sh '''
+                cd ./projects/k3s_cluster_aws/cluster_init/terraform/master_node_config
+                terraform init -input=false
+                terraform plan -destroy -out=terraform_destroy.tfplan
+                '''
+            }
+        }
+        stage('Approval for Destroy Master Node(s)') {
+            steps {
+                input(
+                    message: 'Review the destroy plan. Proceed with destroy?',
+                    ok: 'Proceed'
+                )
+            }
+        }
+        stage('Terraform Apply Destroy Master Node(s)') {
+            steps {
+                sh '''
+                cd ./projects/k3s_cluster_aws/cluster_init/terraform/master_node_config
+                terraform apply -input=false terraform_destroy.tfplan
+                '''
+            }
+        }
+        stage('Terraform Plan Destroy VPC') {
+            steps {
+                sh '''
+                cd ./projects/k3s_cluster_aws/cluster_init/terraform/main_vpc_config
+                terraform init -input=false
+                terraform plan -destroy -out=terraform_destroy.tfplan
+                '''
+            }
+        }
+        stage('Approval for Destroy VPC') {
+            steps {
+                input(
+                    message: 'Review the destroy plan. Proceed with destroy?',
+                    ok: 'Proceed'
+                )
+            }
+        }
+        stage('Terraform Apply Destroy VPC') {
+            steps {
+                sh '''
+                cd ./projects/k3s_cluster_aws/cluster_init/terraform/main_vpc_config
+                terraform apply -input=false terraform_destroy.tfplan
+                '''
+            }
+        }
+    }
+}
