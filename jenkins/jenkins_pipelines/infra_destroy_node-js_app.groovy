2bc51aa75310198b255a3601c1787bd8a218dfe5
OleksiiPasichnyk <axelpasechnik@gmail.com>
2024-02-19 17:54:02 -0800

    updates

--- jenkins/jenkins_pipelines/infra_destroy_node-js_app.groovy
+++ jenkins/jenkins_pipelines/infra_destroy_node-js_app.groovy
@@ -6,12 +6,23 @@ pipeline {
     stages {
         stage('Clone Git repo') {
             steps {
-                git(
-                branch: 'jenkins_instance_setup',
-                url: 'https://github.com/OleksiiPasichnyk/Terraform.git',
-                credentialsId: 'access_to_git'
-                )
-            }
+    // Bind the SSH private key to an environment variable
+    withCredentials([sshUserPrivateKey(credentialsId: 'YourKeyCredNameHere', keyFileVariable: 'SSH_KEY')]) {
+        sh '''
+        # Start the SSH agent and add the key
+        eval $(ssh-agent -s)
+        ssh-add $SSH_KEY
+        
+        # Ensure GitHub is a known host
+        ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+        
+        # Use verbose output for the git command
+        GIT_SSH_COMMAND="ssh -v" git clone -b main --single-branch git@github.com:YourNameHere/DevOps_Jan_24.git
+        
+        # Clean up the SSH agent
+        eval $(ssh-agent -k)
+        '''
+    }
         }
         stage('Terraform Initialize and Plan Destroy') {
             steps {
